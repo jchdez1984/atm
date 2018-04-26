7ed820a332efa3d606236362ca32236f95058966
Juan Hernandez <juanhernandez@Juans-MacBook-Pro.local>
2018-04-26 11:19:40 -0500

    scaffolding to make first scenario pass

--- src/test/java/nicebank/Steps.java
+++ src/test/java/nicebank/Steps.java
@@ -10,25 +10,27 @@ import transforms.MoneyConverter;
 
 public class Steps {
 
+    KnowsMyAccount helper;
+
+    public Steps(){
+        helper = new KnowsMyAccount();
+    }
 
     @Given("^I have deposited \\$(\\d+\\.\\d+) in my account$")
     public void iHaveDeposited$InMyAccount(@Transform(MoneyConverter.class) Money amount) throws Throwable {
-        Account myAccount = new Account();
-        myAccount.deposit(amount);
-
-        Assert.assertEquals("incorrect account balance -", amount, myAccount.getBalance());
+        helper.getMyAccount().deposit(amount);
+        Assert.assertEquals("incorrect account balance - ", amount, helper.getMyAccount().getBalance());
     }
 
-    @When("^I request \\$(\\d+)$")
-    public void iRequest$(int arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @When("^I withdraw \\$(\\d+)$")
+    public void iWithdraw(int amount) throws Throwable{
+        Teller teller = new Teller(helper.getCashSlot());
+        teller.withdrawFrom(helper.getMyAccount(), amount);
     }
 
     @Then("^\\$(\\d+) should be dispensed$")
-    public void $ShouldBeDispensed(int arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void $ShouldBeDispensed(int dollars) throws Throwable {
+        Assert.assertEquals("incorrect amount dispensed -", dollars, helper.getCashSlot().contents());
     }
 
 
@@ -41,6 +43,48 @@ public class Steps {
 
         public Money getBalance() {
             return balance;
+        }
+    }
+
+    class Teller{
+        private CashSlot cashSlot;
+
+        public Teller(CashSlot cashSlot){
+            this.cashSlot = cashSlot;
+        }
+
+        public void withdrawFrom(Account account, int dollars){
+            cashSlot.dispense(dollars);
+        }
+    }
+
+    class CashSlot{
+        private int contents;
+
+        public int contents(){
+            return contents;
+        }
+
+        public void dispense(int dollars) {
+            contents = dollars;
+        }
+    }
+
+    class KnowsMyAccount{
+        private Account myAccount;
+        private CashSlot cashSlot;
+        public Account getMyAccount(){
+            if(myAccount == null){
+                myAccount = new Account();
+            }
+            return myAccount;
+        }
+
+        public CashSlot getCashSlot(){
+            if(cashSlot == null){
+                cashSlot = new CashSlot();
+            }
+            return cashSlot;
         }
     }
 }
